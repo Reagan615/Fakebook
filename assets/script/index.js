77992e05ade5d20fc59e2924116cfd76683bd251
Reagan615 <419247451@qq.com>
2022-11-28 17:02:46 -0600

    done html

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,11 +1,90 @@
 'use strict';
 
-
-let btn = document.querySelector('.search button');
+let info = document.querySelector('.info');
+let span = document.querySelector('.info span');
+let btn = document.querySelector('.post button');
 let grid = document.querySelector('.grid-container');
-let tip = document.querySelector('.msg span');
+let items = document.querySelectorAll('.grid-container div');
+let photo = document.querySelector('.photo');
 
-   
+
+class User {
+    #id;
+    #name;
+    #userName;
+    #email;
+
+    constructor(id, name, userName, email) {
+        this.#id = id;
+        this.#name = name;
+        this.#userName = userName;
+        this.#email = email;
+    }
+
+    get id() {return this.#id};
+    get name() {return this.#name};
+    get userName() {return this.#userName};
+    get email() {return this.#email};
+
+    getInfo() {
+        return `ID:${this.#id}<br>
+                Name:${this.#name}<br>
+                userName:${this.#userName}<br>
+                Email:${this.email}`
+    }
+
+} 
+const user = new User('1234567', 'Jack', 'Ali', '56789@gmail.com');
+
+let flag = 0;
+photo.addEventListener('click', function() {
+    if(flag === 0) {
+        info.style.visibility = 'visible';
+        span.innerHTML = user.getInfo();
+        flag = 1;
+    } else {
+        info.style.visibility = 'hidden';
+        flag = 0;
+    }   
+});
+
+
+let file = document.querySelector('.upfile')
+let nameShow = document.querySelector('.picture span')
+
+file.addEventListener('change', function() {
+    
+    let fileName = this.files[0].name.trim();
+    nameShow.innerHTML = `${fileName}`;
+
+})
+
+
+
+btn.addEventListener('click', function() {
+    let text = document.querySelector('.note textarea').value;
+    let item = document.createElement('div');
+    
+    grid.insertBefore(item, grid.children[0]);
+    item.innerHTML = text;
+
+    let photoFile = file.files;
+    console.log(photoFile);
+    let reader = new FileReader();
+    reader.readAsDataURL(photoFile[0]);
+    reader.onload = function() {
+        let image = document.createElement('img');
+        image.width = '400';
+        image.src = reader.result;
+        let items = document.querySelector('.grid-container div');
+        items.append(image);
+    }
+
+    item.addEventListener('click', function() {
+        grid.removeChild(this);
+    });
+})
+
 
 
 
