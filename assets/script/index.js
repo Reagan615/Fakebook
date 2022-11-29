e7f601398db434370076eefd951a92617a068d71
Reagan615 <419247451@qq.com>
2022-11-28 19:44:40 -0600

    done js

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,8 +6,6 @@ let btn = document.querySelector('.post button');
 let grid = document.querySelector('.grid-container');
 let items = document.querySelectorAll('.grid-container div');
 let photo = document.querySelector('.photo');
-
-
 class User {
     #id;
     #name;
@@ -34,7 +32,8 @@ class User {
     }
 
 } 
-const user = new User('1234567', 'Jack', 'Ali', '56789@gmail.com');
+const user = new User('1234567', 'Jack Ma', 'Ali', '56789@gmail.com');
+
 
 let flag = 0;
 photo.addEventListener('click', function() {
@@ -53,37 +52,41 @@ let file = document.querySelector('.upfile')
 let nameShow = document.querySelector('.picture span')
 
 file.addEventListener('change', function() {
-    
     let fileName = this.files[0].name.trim();
     nameShow.innerHTML = `${fileName}`;
-
-})
-
+});
 
 
+let output = document.querySelector('.grid-container div .text');
 btn.addEventListener('click', function() {
     let text = document.querySelector('.note textarea').value;
     let item = document.createElement('div');
-    
+    let userName = document.querySelector('.userInfo span');
+    let newTime = document.querySelector('.time');
+    let attach = document.querySelector('.attach');
+    let time = new Date();
+
+    userName.innerHTML = user.name;
+    newTime.innerHTML = time.toString().substring(0, 15);
+    output.innerHTML = text;
+    item.innerHTML = attach.innerHTML;
     grid.insertBefore(item, grid.children[0]);
-    item.innerHTML = text;
 
     let photoFile = file.files;
-    console.log(photoFile);
     let reader = new FileReader();
     reader.readAsDataURL(photoFile[0]);
     reader.onload = function() {
         let image = document.createElement('img');
-        image.width = '400';
+        image.width = '500';
         image.src = reader.result;
         let items = document.querySelector('.grid-container div');
         items.append(image);
     }
-
+    
     item.addEventListener('click', function() {
         grid.removeChild(this);
     });
-})
+});
 
 
 
